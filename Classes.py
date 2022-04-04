eeae5cf3889b03cfd459e0b6b881196d23dd6861
Ryan Wong <ryanwong1110@yahoo.com>
2022-04-04 15:59:23 +0800

    .

--- Classes.py
+++ Classes.py
@@ -37,6 +37,43 @@ class Thompson2:
         
         return self.bandits[bandit], reward
 
+class Parameters:
+    def __init__(self):
+        self.n = 0
+        self.x = []
+        self.a = 1
+        self.b = 1
+        self.mu = 1
+        self.sum = 0
+    
+    def update(self, x):
+        self.n += 1
+        self.sum += x
+        self.x.append(x)
+        self.mu = self.sum/self.n
+        v = self.n
+
+        self.a += 1/2
+        self.b += ((v/(v + 1)) * (((x - self.mu)**2)/2))
+
+    def __str__(self) -> str:
+        return f'Alpha: {self.a}  Beta: {self.b}  Mu: {self.mu}  v: {self.n} est : {np.mean([np.random.gamma(self.a, 1/self.b) for _ in range(10)])}'
+        
+
+
+class Thompson3:
+    name = "Thompson3"
+    def __init__(self, bandits):
+        self.num = len(bandits)
+        self.bandits = bandits
+        self.data = [Parameters() for _ in range(self.num)]
+    def pull(self):
+        bandit = max(range(self.num), key=lambda n:
+            np.random.normal(self.data[n].mu, np.sqrt(1/np.random.gamma(self.data[n].a, 1/self.data[n].b))))
+        r = self.bandits[bandit].pull()
+        self.data[bandit].update(r)
+        return self.bandits[bandit], r
+
 class Random:
     name = "Random"
     def __init__(self, bandits):
